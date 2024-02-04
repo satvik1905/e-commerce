6a3d7e02d16d301048479e09cb0a8eae2c90cb41
Satvik Khopkar <skhopkar@csumb.edu>
2024-02-04 00:12:52 -0500

    exactly what we need for Monday

---
+++ ecommerce/src/Pages/Shops.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../Components/Hero/Hero";
+
+const Shops = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Shops;
